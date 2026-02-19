fd26a2d0718e25acd41341cc079a1a2223d47b4a
advaithProjects <advaithvijjagiri@gmail.com>
2026-02-19 14:46:11 +0530

    19/02/2026 2:50pm commit

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.css";
-import Sidebar from "../Components/Sidebar";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+
 const Home = () => {
   return (
     <>
