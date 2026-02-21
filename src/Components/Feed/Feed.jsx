50590e78bec900e9a889929e46b009899b726329
advaithProjects <advaithvijjagiri@gmail.com>
2026-02-21 20:44:08 +0530

    21/02/2026 8:44pm

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -8,17 +8,18 @@ import thumbnail5 from "../../assets/thumbnail5.png";
 import thumbnail6 from "../../assets/thumbnail6.png";
 import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
+import { Link } from "react-router-dom";
 const Feed = () => {
   return (
     <div className="feed">
-      <div className="card">
+      <Link to={`video/20/4521`} className="card">
         <img src={thumbnail1} alt="" />
         <h2>
           Best Channal to Learn coding that help you to be a web developer
         </h2>
         <h3>Greatstack</h3>
         <p>15k views &bull; 2 days ago</p>
-      </div>
+      </Link>
       <div className="card">
         <img src={thumbnail2} alt="" />
         <h2>
@@ -76,7 +77,7 @@ const Feed = () => {
         <p>15k views &bull; 2 days ago</p>
       </div>
     </div>
-  )
+  );
 };
 
 export default Feed;
