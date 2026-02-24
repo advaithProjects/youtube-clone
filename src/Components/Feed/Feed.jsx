27ed0c840cd1f54e9bd64f265441d9ac1d29ae03
advaithProjects <advaithvijjagiri@gmail.com>
2026-02-24 15:54:21 +0530

    24/02/2026 3:55pm commit

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -9,11 +9,12 @@ import thumbnail6 from "../../assets/thumbnail6.png";
 import thumbnail7 from "../../assets/thumbnail7.png";
 import thumbnail8 from "../../assets/thumbnail8.png";
 import { Link } from "react-router-dom";
-import { API_KEY } from "../../data";
+import { API_KEY, value_converter } from "../../data";
+import moment from "moment/moment";
 const Feed = ({ category }) => {
   const [data, setData] = useState([]);
   const fetchData = async () => {
-    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&videoCategoryId=${category}&key=${API_KEY}`;
+    const videoList_url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&videoCategoryId=${category}&key=${API_KEY}`;
     await fetch(videoList_url)
       .then((response) => response.json())
       .then((data) => setData(data.items));
@@ -23,15 +24,19 @@ const Feed = ({ category }) => {
   }, [category]);
   return (
     <div className="feed">
-      {data.map((items, index) => {
+      {data.map((item, index) => {
         return (
-          <Link to={`video/20/4521`} className="card">
-            <img src={thumbnail1} alt="" />
-            <h2>
-              Best Channal to Learn coding that help you to be a web developer
-            </h2>
-            <h3>Code Spark</h3>
-            <p>15k views &bull; 2 days ago</p>
+          <Link
+            to={`video/${item.snippet.categoryId}/${item.id}`}
+            className="card"
+          >
+            <img src={item.snippet.thumbnails.medium.url} alt="" />
+            <h2>{item.snippet.title}</h2>
+            <h3>{item.snippet.channelTitle}</h3>
+            <p>
+              {value_converter(item.statistics.viewCount)} views &bull;
+              {moment(item.snippet.publishedAt).fromNow()}
+            </p>
           </Link>
         );
       })}
